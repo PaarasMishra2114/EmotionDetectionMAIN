4784ac70629da85d643e0d6ad90e94fcef0addc1
PaarasMishra2114 <0902cs231066@rjit.ac.in>
2026-09-03 23:33:54 +0530

    Add Hugging Face deployment configuration

--- server.py
+++ server.py
@@ -67,4 +67,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)), debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 7860)), debug=False)
